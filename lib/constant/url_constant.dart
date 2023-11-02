208980f86d33c8689e23e7d6187daee5ecc92e3d
duyquan3112 <kiliva3112@gmail.com>
2023-11-02 17:09:15 +0700

    fix conflict

--- lib/constant/url_constant.dart
+++ lib/constant/url_constant.dart
@@ -1,3 +1,16 @@
 //sửa ip thì nhớ sửa cái link lại rồi mới xài được nhéee
 
 final String login_url = "http://192.168.1.135:8000/api/Users/login";
+
+//mang nha quan
+//final String login_url = "http://192.168.1.12:8000/api/Users/login";
+
+// final String login_url = "http://192.168.1.62:8000/api/Users/login";
+
+//cai link nay la mang o truong, o truong co nhieu link lam
+//final String login_url = "http://10.102.67.149:8000/api/Users/login";
+
+//link nay mang nha tao
+//final String login_url = "http://192.168.1.199:8000/api/Users/login";
+
+final String base_url = "http://192.168.1.199:8000/";
