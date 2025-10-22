b1d40d6c539d09f62c460179040196e73de3f11b
th50989 <th50989@gmail.com>
2025-10-22 21:20:35 +0700

    test git

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
 
-  print("Handling  background messages: ${message.messageId}");
+  print("Handling a background messages: ${message.messageId}");
 }
 
 void main() async {
